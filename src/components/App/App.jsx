0f063a1158e7d1b35acc0210d6968ea45995f223
Andriy Ivanenko <andriy.ivanenko@gmail.com>
2022-10-20 00:40:55 +0300

    done

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -7,14 +7,28 @@ import Filter from 'components/Search/Filter';
 
 class PhoneBook extends React.Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    // contacts: [
+    //   { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+    //   { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+    //   { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+    //   { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    // ],
+    contacts: [],
     filter: '',
   };
+
+  componentDidMount() {
+    const storedContacts = JSON.parse(localStorage.getItem('contacts'));
+    if (storedContacts && storedContacts.length > 0) {
+      this.setState({ contacts: storedContacts });
+    }
+  }
+
+  componentDidUpdate(prevState) {
+    if (this.state.contacts !== prevState.concats) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
 
   formSubmitHandler = data => {
     if (this.state.contacts.find(contact => contact.name === data.name)) {
